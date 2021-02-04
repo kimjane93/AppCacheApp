0c2389254ddd31a4464e339660d480a2763eb43e
kimjane93 <kjchadwell93@gmail.com>
2021-02-04 10:51:44 -0500

    import .env in settings

--- appcache/settings.py
+++ appcache/settings.py
@@ -24,6 +24,11 @@ import environ
 environ.Env()
 environ.Env.read_env()
 
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
